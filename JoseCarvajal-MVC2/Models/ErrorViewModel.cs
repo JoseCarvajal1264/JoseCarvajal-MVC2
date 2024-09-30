2dbe77494c535becf54d32ec684c8900bd1132b5
laboratorio <laboratorio@UPOAULA10215>
2024-09-30 07:31:01 -0500

    Agregar archivos de proyecto.

---
+++ JoseCarvajal-MVC2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoseCarvajal_MVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
